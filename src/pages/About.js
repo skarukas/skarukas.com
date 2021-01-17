f9bbb7a3c11f00a45ac57d9f69451054dad9c56a
Stephen Karukas <stephen.karukas@gmail.com>
2021-01-17 14:26:01 -0500

    compress About images

--- src/pages/About.js
+++ src/pages/About.js
@@ -35,8 +35,8 @@ export default class AboutPage extends React.Component {
             <div id="about-page">
                 <div className="bio-container">
                     <div className="bio-image-container">
-                        <img className="bio-image light" src="img/about-pic-light.png" alt="Stephen Karukas existing on a couch"/>
-                        <img className="bio-image dark" src="img/about-pic-dark.png" alt="Stephen Karukas having an existential crisis on a couch"/>
+                        <img className="bio-image light" src="img/about-pic-light.jpg" alt="Stephen Karukas existing on a couch"/>
+                        <img className="bio-image dark" src="img/about-pic-dark.jpg" alt="Stephen Karukas having an existential crisis on a couch"/>
                     </div>
 
                     <CompositionBio />
