43c44973b4f2aa0a21403f8806272fda94a5b6e0
Stephen Karukas <stephen.karukas@gmail.com>
2025-02-26 08:39:27 -0800

    Add wayward shows

--- src/pages/MixedProjects.js
+++ src/pages/MixedProjects.js
@@ -1,58 +1,49 @@
 import React from "react"
-import $ from "jquery"
 import EasyLink from "../components/EasyLink"
-import CollagesPage from "./Collages"
-
-const MONTHS = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
 
 const SYMBOLS = ["☰", "☱", "☲", "☳", "☴", "☵", "☶"].sort(
   () => Math.random() - 0.5
 )
 
-class ProjectInfo {
-  constructor(year, month, title, content = null, url = null, custom_time = false) {
-    this.year = year
-    this.month_n = MONTHS.indexOf(month)
-    let time = custom_time === false ? month + " " + year : custom_time
-    content = content ? " — " + content : ''
-    let timestr = time ? " (" + time + ")" : ''
-    this.element = (<span>
-      <EasyLink to={url}><strong>{title}</strong>{timestr}</EasyLink>
-      {content}
-    </span>)
-  }
+function ProjectInfo(props) { //year, month, title, content = null, url = null, custom_time = false) {
+  let time = props.month + " " + props.year
+  let content = props.content ? " — " + props.content : ''
+  let timestr = time ? " (" + time + ")" : ''
+  return (<span>
+    <EasyLink to={props.url}><strong>{props.title}</strong>{timestr}</EasyLink>
+    {content}
+  </span>)
 }
 
 const projectsData = [
-  new ProjectInfo(
-    2023,
-    'april',
-    'city of water',
-    '[as kmodp] collaborative ep with cellartone and luc jardie.',
-    "https://cellartone.bandcamp.com/album/city-of-water-2"
-  ),
-  new ProjectInfo(
-    2024,
-    'february',
-    'crimée no. 7',
-    '[as kmodp] album of electronic music.',
-    "https://kmodp.net/#/crimee-no-7"
-  ),
-  new ProjectInfo(
-    2024,
-    'february',
-    'rain on the sea',
-    "single by Traveling Boyfriend (arrangement / production)",
-    'https://travelingboyfriend.bandcamp.com/track/rain-on-the-sea'
-  ),
-].sort((a, b) => {
-  if (a.year === b.year) {
-    return b.month_n - a.month_n
-  }
-  return b.year - a.year
-})
+  (<span>
+    September 20, 2025 — Splinter Percussion @ <EasyLink to={"https://www.waywardmusic.org/"}>Wayward Music Series.</EasyLink>
+  </span>),
+  (<span>
+    March 21, 2025 — <EasyLink to={"https://www.waywardmusic.org/event/bongo-funhouse/"}>Bongo Funhouse (improvised percussion trio) @ Wayward Music Series.</EasyLink>
+  </span>),
+  (<ProjectInfo
+    year={2024}
+    month={'february'}
+    title={'crimée no. 7'}
+    content={'[as kmodp] album of electronic music.'}
+    url={"https://kmodp.net/#/crimee-no-7"} />)
+  ,
+  (<ProjectInfo
+    year={2024}
+    month={'february'}
+    title={'rain on the sea'}
+    content={'single by Traveling Boyfriend (arrangement / production).'}
+    url={"https://travelingboyfriend.bandcamp.com/track/rain-on-the-sea"} />),
+  (<ProjectInfo
+    year={2023}
+    month={'april'}
+    title={'city of water'}
+    content={'[as kmodp] collaborative ep with cellartone and luc jardie.'}
+    url={"https://cellartone.bandcamp.com/album/city-of-water-2"} />),
+]
 
-/* Display the list of CS projects */
+/* Display the list of projects */
 export default class MixedProjectsPage extends React.Component {
   state = { data: projectsData }
   render() {
@@ -73,7 +64,6 @@ export default class MixedProjectsPage extends React.Component {
 
 function Project(props) {
   let symbol = SYMBOLS[props.idx % SYMBOLS.length]
-  return (<li bullet-style={symbol} className="fade-on-scroll">{props.data.
-    element}
+  return (<li bullet-style={symbol} className="fade-on-scroll">{props.data}
   </li>)
 }
