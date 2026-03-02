7a9e1c7077d5dbdfb6d7fe1c5f1da790f323f572
Stephen Karukas <stephen.karukas@gmail.com>
2026-03-02 00:09:03 +0000

    Update news

--- src/pages/MixedProjects.js
+++ src/pages/MixedProjects.js
@@ -1,5 +1,7 @@
 import React from "react"
 import EasyLink from "../components/EasyLink"
+import WorksPage from "./Works"
+
 
 const SYMBOLS = ["☰", "☱", "☲", "☳", "☴", "☵", "☶"].sort(
   () => Math.random() - 0.5
@@ -17,45 +19,57 @@ function ProjectInfo(props) { //year, month, title, content = null, url = null, 
 
 const projectsData = [
   (<span>
-    April 29, 2026 — Bongo Funhouse (improvised percussion + electronics) @ Wayward Music Series.
+    🥁 April 29, 2026 — Bongo Funhouse (improvised percussion + electronics) @ Wayward Music Series (Seattle).
   </span>),
   (<span>
-    December 2025 — <EasyLink to="https://pdimagearchive.org/images/465beb37-7ca3-4ece-9e34-cdeb836d586f/">see</EasyLink> you later, <b>I'm moving to <EasyLink to="https://pdimagearchive.org/images/c71753a0-8c40-4e41-b6fd-a5af3bb28f50/">London</EasyLink></b> to help self-driving <EasyLink to="https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-12561-1fmrojp_93d41eea.jpeg?region=0,0,400,225">cars</EasyLink> drive themselves better.
+    🥁 April 24, 2026 — <EasyLink to="https://www.splinterpercussion.org">Splinter Percussion</EasyLink> live radio show on <EasyLink to="https://classicalking.org/show/nw-focus-live">Classical KING Northwest Focus Live</EasyLink> (Seattle).
   </span>),
   (<span>
-    September 20, 2025 — <EasyLink to="https://www.waywardmusic.org/event/splinter-percussion/">Splinter Percussion @ Wayward Music Series.</EasyLink>
+    🎷 March 2026 — Premiere of new bari sax + electronics arrangement of <EasyLink to={WorksPage.workUrl("third-rail-electronics")}><em>Third Rail / Revelation</em></EasyLink> by <EasyLink to="https://www.andrew-hutchens.com/events">Andrew Hutchens</EasyLink> at NASA 2026.
   </span>),
   (<span>
-    August 29, 2025 — Splinter Percussion @ <EasyLink to="https://www.livemusicproject.org/events/158451/live-music-project-11th-birthday-bash-feat-isabel-hagen">Live Music Project's 11th Anniversary</EasyLink>
+    💂 December 2025 — Hey, I moved to <EasyLink to="https://pdimagearchive.org/images/c71753a0-8c40-4e41-b6fd-a5af3bb28f50/">London</EasyLink> to help self-driving <EasyLink to="https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-12561-1fmrojp_93d41eea.jpeg?region=0,0,400,225">cars</EasyLink> drive themselves better.
   </span>),
   (<ProjectInfo
     year={2025}
-    month={'march'}
-    title={'inconnu'}
+    month={'November'}
+    title={'💿 Girlhood'}
+    content={'single by Traveling Boyfriend (arrangement / production).'}
+    url={"https://travelingboyfriend.bandcamp.com/track/girlhood-2"} />),
+  (<span>
+    🥁 September 20, 2025 — <EasyLink to="https://www.waywardmusic.org/event/splinter-percussion/">Splinter Percussion @ Wayward Music Series.</EasyLink>
+  </span>),
+  (<span>
+    🥁 August 29, 2025 — Splinter Percussion @ <EasyLink to="https://www.livemusicproject.org/events/158451/live-music-project-11th-birthday-bash-feat-isabel-hagen">Live Music Project's 11th Anniversary</EasyLink>
+  </span>),
+  (<ProjectInfo
+    year={2025}
+    month={'March'}
+    title={'💿 [inconnu]'}
     content={'electronic music collaboration between kmodp and Køemner.'}
     url={"https://open.spotify.com/track/3lkEvOKH2c3w8oJErmS8Nl?si=ab029a55a1eb4873"} />)
   ,
   (<span>
-    March 21, 2025 — <EasyLink to={"https://www.waywardmusic.org/event/bongo-funhouse/"}>Bongo Funhouse (improvised percussion trio) @ Wayward Music Series.</EasyLink>
+    🥁 March 21, 2025 — <EasyLink to={"https://www.waywardmusic.org/event/bongo-funhouse/"}>Bongo Funhouse (improvised percussion trio) @ Wayward Music Series.</EasyLink>
   </span>),
   (<ProjectInfo
     year={2024}
-    month={'february'}
-    title={'crimée no. 7'}
+    month={'February'}
+    title={'💿 Crimée No. 7'}
     content={'[as kmodp] album of electronic music.'}
     url={"https://kmodp.net/#/crimee-no-7"} />)
   ,
   (<ProjectInfo
     year={2024}
-    month={'february'}
-    title={'rain on the sea'}
+    month={'February'}
+    title={'💿 Rain on the Sea'}
     content={'single by Traveling Boyfriend (arrangement / production).'}
     url={"https://travelingboyfriend.bandcamp.com/track/rain-on-the-sea"} />),
   (<ProjectInfo
     year={2023}
-    month={'april'}
-    title={'city of water'}
-    content={'[as kmodp] collaborative ep with cellartone and luc jardie.'}
+    month={'April'}
+    title={'💿 City of Water'}
+    content={'[as kmodp] collaborative EP with Cellartone and Luc Jardie.'}
     url={"https://cellartone.bandcamp.com/album/city-of-water-2"} />),
 ]
 
